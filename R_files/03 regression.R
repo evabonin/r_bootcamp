6e7d81162e702520f99b9f599a0c105bf21edec6
Eva Bonin <eva00unterwegs@gmail.com>
2023-03-11 20:24:57 +0100

    additional work on reg

--- R_files/03 regression.R
+++ R_files/03 regression.R
@@ -20,33 +20,46 @@ library(ggplot2)
 
 # Does the suicide rate increase over time for males or females?
 
-reg1 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + year)
-reg2 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + year | country)
+# Simplest model
+reg1 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + year + sex*year)
+
+# Fixed effect by country
+reg2 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + year + sex*year | country)
+
+# Including economic data
+reg3 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + year + sex*year + pop_t + 
+                                                            edu + gdp_pc + unem_y + unem_t | country)
+
+# Including mental health data
+reg4 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + year + sex*year + pop_t 
+                                                          + edu + gdp_pc + unem_y + unem_t+ 
+                                                            alc + drug + depr + sh | country)
+
+# 4 plus fixed effect for year
+reg5 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + pop_t 
+                                                          + edu + gdp_pc + unem_y + unem_t+ 
+                                                            alc + drug + depr + sh | country + year,
+                                                          cluster = ~ country)
+# 5 minus vars with zero coefficient
+reg6 <- suicide_final %>% filter(sex != "Both") %>% feols(sui ~ sex + 
+                                                          edu + unem_y + unem_t+ 
+                                                            alc + drug + depr + sh | country + year,
+                                                          cluster = ~ country)
 
 
+models <- list(reg1, reg2, reg3, reg4, reg5, reg6)
 
-modelsummary(reg2)
-
-# plot regression lines
-
-reg_plot1 <- suicide_final %>% filter(sex != "Both") %>%
-  ggplot(aes(x = sui, y = sex)) +
-  geom_point() +
-  geom_smooth(method = "lm", fill = NA)
-
-reg_plot1
-
-
-
-
-
-models <- list(reg1, reg2)
+coefs = c("sexMale" = "Male", "year" = "Year", "sexMale year" = "Male * Year", "pop_t" = "Total population", "edu" = "Years compulsory education", "gdp_pc" = "GDP per capita", "unem_y" = "Youth unemployment", "unem_t" = "Total unemployment", "alc" = "Alcohol misuse", "drug" = "Drug misuse", "sh" = "Self harm")
 
 modelsummary(models,
+             coef_omit = "Intercept",
+             coef_map = coefs,
              stars = TRUE)
 
 
-modelplot(list(reg1, reg2),
-          guides(color = guide_legend(reverse = TRUE)))
+modelplot(models,
+          coef_map = c("sexMale" = "Male")) +
+          guides(color = guide_legend(reverse = TRUE))
+
 
 
